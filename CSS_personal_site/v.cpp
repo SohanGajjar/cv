9025fb381856a7664d75ba582549fde8fb7fe0bf
sohan <70092455+SohanGajjar@users.noreply.github.com>
2021-08-02 14:36:23 +0530

    Add files via upload

---
+++ CSS_personal_site/v.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    if((n/2)*2 == n) 
+    {
+        cout << ':' << endl;
+    
+    }
+    else
+    cout << "g" << endl;
+return 0;
+}
